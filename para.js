f6d5e28f2fdc96f47df4dc2bbdb881ce7a7856e9
KMONISH <monishkb1999@gmail.com>
2019-07-21 23:13:36 +0530

    para.js

---
+++ para.js
@@ -0,0 +1,12 @@
+<script type = "text/javascript"> 
+
+// Function definition 
+function welcomeMsg(name) { 
+document.write("Hello " + name +); 
+} 
+
+var nameVal = "raja sir"; 
+
+welcomeMsg(nameVal); 
+
+</script> 
