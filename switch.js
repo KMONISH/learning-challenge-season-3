1a456d809c1fb3186c3ce17472be45f40c3950c6
KMONISH <monishkb1999@gmail.com>
2019-07-21 23:08:43 +0530

    switch.js

---
+++ switch.js
@@ -0,0 +1,20 @@
+<script type = "text/javascript"> 
+
+	var i = 9; 
+
+	switch (i) 
+	{ 
+	case 0: 
+		document.write("i is zero."); 
+		break; 
+	case 1: 
+		document.write("i is one."); 
+		break; 
+	case 2: 
+		document.write("i is two."); 
+		break; 
+	default: 
+		document.write("i is greater than 2."); 
+	} 
+
+</script></div> 
