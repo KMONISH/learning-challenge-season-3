09635e456df26bc95e0f3aa974bf87faa61fbe96
KMONISH <monishkb1999@gmail.com>
2019-07-21 23:03:28 +0530

    ifcon.js

---
+++ ifcon.js
@@ -0,0 +1,13 @@
+<script type = "text/javaScript">  
+
+var i = 20; 
+
+if (i == 10) 
+document.wrte("i is 10"); 
+else if (i == 15) 
+document.wrte("i is 15"); 
+else if (i == 20) 
+document.wrte("i is 20"); 
+else
+document.wrte("i is not present"); 
+< /script> 
